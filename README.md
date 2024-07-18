896985dbfee71a9933dffa7278b0b045f0c81d30
saurabhpanth <saurabhpanth26@gmail.com>
2024-07-19 01:14:07 +0530

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # docker_multi_container_project
+
+
+1. Run docker continer of mongo
+
+2. Run backend container
+
+3. Run frontend conatiner
--- backend/app.js
+++ backend/app.js
@@ -84,7 +84,7 @@ app.delete('/goals/:id', async (req, res) => {
 });
 
 mongoose.connect(
-  'mongodb://host.docker.internal:27017/course-goals',
+  'mongodb://172.17.0.2:27017/course-goals',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
